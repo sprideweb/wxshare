17fad4dc0b75a600d29329d1bf9ac42a7efa7281
sprideweb <22722033@bjtu.edu.cn>
2025-05-22 20:42:46 +0800

    1.

---
+++ head.js
@@ -0,0 +1,20 @@
+document.getElementById('top').innerHTML=`
+<nav class="navbar navbar-expand bg-light">
+	<div class="container">
+		<a class="navbar-brand" href="/"><img src="https://13511974.s21i.faiusr.com/4/ABUIABAEGAAggYq8wQYouNnfgAIwsAk4sAk.png" height="24px"/>&nbsp;春潮分享 Share</a>
+		<ul class="navbar-nav">
+			<li class="nav-item"><a class="nav-link" href="index.html">首页 Home</a></li>
+			<li class="nav-item"><a class="nav-link" href="news.html">新闻 News</a></li>
+			<li class="nav-item"><a class="nav-link" href="about.html">关于 About</a></li>
+			<li class="nav-item"><a class="nav-link" href="join.html">入驻 Join</a></li>
+		</ul>
+	</div>
+</nav>
+`;
+document.getElementById('btm').innerHTML=`
+<div class="container text-center">
+	<hr/>
+	<p><img src="https://13511974.s21i.faiusr.com/4/ABUIABAEGAAggYq8wQYouNnfgAIwsAk4sAk.png" style="filter:grayscale(100%);height:24px;"/></p>
+	<p>Powered by © 2022 大爱无疆 <a href="sitevalue.html">本站统计</a></p>
+</div>
+`;
